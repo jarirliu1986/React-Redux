df75300efdf39f81d7f1eb01ad6fa4067059a6a3
jarirliu1986 <289374247@qq.com>
2017-05-06 21:51:37 -0700

    first search on youtube, delete previous test comment

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component{
-	render(){
-		return <input />;
+	constructor(props){
+		super(props);
+
+		this.state = {term : ''};
 	}
+
+	render(){
+		//return <input onChange = {this.onInputChange} />;
+		return (
+			<div>
+				<input value={this.state.term} 
+				 onChange = {event => this.setState({term : event.target.value})} />
+			</div>
+		); 
+		
+	}
+
+	// onInputChange(event){
+	// 	console.log(event.target.value);
+	// }
 }
 
 // const SearchBar = () => {
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyAA7kbDSntFiPyO4yV0rJzR7vOswshnebg';
-//Create new component to produce HTML
 
+//Create new component to produce HTML
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
+	console.log(data);
+});
 const App = () => {
 	return (
 		<div>
